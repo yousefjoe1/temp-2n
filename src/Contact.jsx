5de45463722fcc1c50ab6372fcd95876d3bbca44
youssefmahmoud <yousefmahmoud150@gmail.com>
2021-12-27 17:32:54 +0200

    commit all2

---
+++ src/Contact.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FcAssistant } from "react-icons/fc";
+import { BsTelephoneForwardFill } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+const Contact = () => {
+    return (
+        <React.Fragment>
+            <div id='contact' className="contact-cont container-fluid">
+                <h2 className='text-center fs-1 fw-bold' >Contact Us</h2>
+                <h3 className='text-center fs-1' ><FcAssistant /></h3>
+
+                <div className="row p-2 mt-4 text-center">
+                    <div className="col-lg-8 col-xl-8 col-md-8 col-sm-12">
+
+                        <div className="form border p-1 rounded border-3 border-light">
+                            <label className='fs-4 fw-bold' htmlFor="email">Email</label><br/>
+                            <input id='email' type="email"/><br/>
+                            <label className='fs-4 fw-bold' htmlFor="password">password</label><br/>
+                            <input id='password' type="password" name="" id=""/><br/>
+                            <textarea placeholder='Your message' name="" id="" cols="30" rows="20"></textarea><br/>
+                            <button className='btn btn-success fs-3' >Send</button>
+                        </div>
+
+                    </div>
+
+                    <div className="col-lg-4 mt-5 col-xl-4 col-md-4 col-sm-12">
+                            <p className='fs-3 text-light fw-bold' ><BsTelephoneForwardFill /> Phone1: 0000000000</p>
+                            <p className='fs-3 text-light fw-bold'><BsTelephoneForwardFill /> Phone2: 0000000000</p>
+                            <p className='fs-3 text-light fw-bold'><BsWhatsapp/> WhatsApp 000000000000 </p>
+                        </div>
+                </div>
+
+            </div>
+        </React.Fragment>
+    );
+}
+ 
+export default Contact;
---
+++ src/Pricing.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { FcCurrencyExchange } from "react-icons/fc";
+
+
+const Pricing = () => {
+    return (
+        <React.Fragment>
+            <div id='price' className="price-cont p-2 mt-4 container-fluid">
+                <h2 className='text-center fw-bold fs-2'>Pricing</h2>
+                <h3  className='text-center fs-1' ><FcCurrencyExchange/></h3>
+                <p className='text-center fs-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iure possimus a ipsa blanditiis.</p>
+
+                <div className="price-div mt-3">
+
+                    <div className="row p-4">
+
+                        <div className="col-lg-3 price-col p-2 text-center border-dark col-md-6 col-sm-12">
+                            <p className='fs-3 fw-bold ' >Start</p>
+                            {/* <hr/> */}
+                            <div className="span-cont p-2">
+                                <span><sup className='fs-5' >$</sup></span>
+                                <span className='fw-bold fs-1' >16</span>
+                                <span>/Mo</span>
+                            </div>
+                            <hr/>
+                            <h3>Future 1</h3>
+                            <h3>Extra Future</h3>
+                            <h3>Extra Future 2</h3>
+                            <h3>Extra Future 3</h3>
+                            <button className='btn btn-primary'>Buy Now</button>
+                        </div>
+
+                        <div className="col-lg-3 price-col p-2 text-center border-dark col-md-6 col-sm-12">
+                            <p className='fs-3 fw-bold' >Premium</p>
+                            {/* <hr/> */}
+                            <div className="span-cont p-2">
+                                <span><sup className='fs-5' >$</sup></span>
+                                <span className='fw-bold fs-1' >22</span>
+                                <span>/Mo</span>
+                            </div>
+                            <hr/>
+                            <h3>Future 1</h3>
+                            <h3>Extra Future</h3>
+                            <h3>Extra Future 2</h3>
+                            <h3>Extra Future 3</h3>
+                            <button className='btn btn-primary'>Buy Now</button>
+                        </div>
+
+                        <div className="col-lg-3 price-col p-2 text-center border-dark  col-md-6 col-sm-12">
+                            <p className='fs-3 fw-bold' >Pro</p>
+                            {/* <hr /> */}
+                            <div className="span-cont p-2">
+                                <span><sup className='fs-5' >$</sup></span>
+                                <span className='fw-bold fs-1' >35</span>
+                                <span>/Mo</span>
+                            </div>
+                            <hr/>
+                            <h3>Future 1</h3>
+                            <h3>Extra Future</h3>
+                            <h3>Extra Future 2</h3>
+                            <h3>Extra Future 3</h3>
+                            <button className='btn btn-primary'>Buy Now</button>
+                        </div>
+
+                        <div className="col-lg-3 price-col p-2 text-center border-dark col-md-6 col-sm-12">
+                            <p className='fs-3 fw-bold' >Platinum</p>
+                            {/* <hr/> */}
+                            <div className="span-cont p-2">
+                                <span><sup className='fs-5' >$</sup></span>
+                                <span className='fw-bold fs-1' >41</span>
+                                <span>/Mo</span>
+                            </div>
+                            <hr/>
+                            <h3>Future 1</h3>
+                            <h3>Extra Future</h3>
+                            <h3>Extra Future 2</h3>
+                            <h3>Extra Future 3</h3>
+                            <button className='btn btn-primary'>Buy Now</button>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+ 
+export default Pricing;
